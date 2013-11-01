634e6b1017942587a3f6c3a7e811eb4d18c6a820
nkammah <nkammah@etsy.com>
2013-11-01 16:57:28 +0000

    follow location (was breakin on JIRA redirects)

--- phplib/CurlClient.php
+++ phplib/CurlClient.php
@@ -3,12 +3,15 @@
 class CurlClient {
 
     function get($url, array $params = null, $user_pass = null) {
-    $query_string = empty($params)
-    ? ''
-    : '?' . http_build_query($params);
+        $query_string = empty($params)
+            ? ''
+            : '?' . http_build_query($params);
         $ch = curl_init($url . $query_string);
-        $options = array(CURLOPT_HTTPGET => true,
-                        CURLOPT_RETURNTRANSFER => 1);
+        $options = array(
+            CURLOPT_HTTPGET => true,
+            CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_RETURNTRANSFER => 1
+        );
         if ($user_pass) {
             $options[CURLOPT_USERPWD] = $user_pass; 
         }
