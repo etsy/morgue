f28acf619e44d4ad9dd24066f21b18922a40e21e
Jeff Kolber <jkolber@etsy.com>
2014-05-02 15:41:12 +0000

    refactors the anniversary code; added a view that mails out an mail-friendly anniversary announcement that links back to morgue.

--- features/anniversary/views/anniversary.php
+++ features/anniversary/views/anniversary.php
@@ -2,37 +2,6 @@
 /**
  * Today in Post Mortem History
  */
-function render_tags($tags) {
-    $out = '';
-    foreach ($tags as $tag) {
-        $out .= "<span class='label tag' id='tag-" .$tag['id'] . "'>{$tag['title']}</span>";
-    }
-    return $out;
-}
-function render_outage($pm) {
-    if (empty($pm['title'])) {
-        $pm['title'] = "untitled";
-    }
-    $nice_start = date("r", $pm['starttime']);
-    $nice_end= date("r", $pm['endtime']);
-    $nice_tags = render_tags($pm['tags']);
-
-    if (!$nice_tags) {
-        $nice_tags = "This event has no tags";
-    }
-    $out = <<<HTML
-    <div>
-    <strong><a href="/events/{$pm['id']}">{$pm['title']}</a></strong><br />
-    {$nice_start} to {$nice_end}  <br />
-    Severity: {$pm['severity']} <br />
-
-    <div id='tag_well' class='well well-small'>$nice_tags</div>
-
-    </div>
-    <hr />
-HTML;
-    return $out;
-}
 
 $count = count($pms);
 if ($count) {
@@ -43,7 +12,7 @@ if ($count) {
     }
     echo "<h3>$human_readable_date is the anniversary of {$outage}.</h3>";
     foreach ($pms as $k => $v) {
-        print render_outage($v);
+        print Anniversary::render_outage($v);
     }
 } else {
     echo "<p>Nothing broke on $human_readable_date.</p>";
