5f92a06220561a7408a05fe7b8a1270fb75f654c
CyberRodent <fej.reblok@gmail.com>
2015-03-10 08:29:54 -0400

    setting up for part 2

--- features/upload/routes.php
+++ features/upload/routes.php
@@ -33,7 +33,11 @@ $app->get('/upload/css/:path' , function ($path) use ($app) {
 });
 
 
-// This is for images coming from dropzone
+/*
+ * This is to handle images coming in via our dropzone
+ *
+ * We assume that these images are to be associated with an event.
+ */
 $app->post('/upload/:id', function($id) use ($app) {
 	$ds = DIRECTORY_SEPARATOR; 
 
@@ -41,6 +45,7 @@ $app->post('/upload/:id', function($id) use ($app) {
 	$upload_base_path = $config['upload_dir'];
 
 	if (!empty($_FILES)) {
+		// Step One: Put our uploaded files somewhere
 		$tempFile = $_FILES['file']['tmp_name'];
 		$targetPath = "{$upload_base_path}{$ds}{$id}{$ds}";
 		// Ensure we have somewhere to upload to
@@ -48,13 +53,25 @@ $app->post('/upload/:id', function($id) use ($app) {
 		// we're making directories here
 		shell_exec("mkdir -p $targetPath");
 		$targetFile =  $targetPath. $_FILES['file']['name'];
+
 		if ( ! move_uploaded_file($tempFile,$targetFile) ) {
 			$app->getLog()->error("Error saving uploaded file.");
 			$app->response()->status(500);
 		}
+
+		$app->getLog()->error("File Uploaded");
+		$options = Configuration::get_configuration('upload');
+
+		// Step Two: Send the file somewhere and expect a URL back
+		$uploader = new Uploader($targetFile, $options);
+
+		// Step Three: Add the URL of the file as an image for the event
+
+
+
 	} else {
 		$app->getLog()->error("Nothign to upload.");
-		$app->response()->status(500);
+		$app->response()->status(400);
 		return;
 	} 
 });
