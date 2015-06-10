f23f9fa2c86afd192729119502473d376cc27507
Matthew Mercedes <mmercedes@etsy.com>
2015-06-10 15:33:57 +0000

    reverting the move of summary js into features folder

--- htdocs/assets/js/edit.js
+++ htdocs/assets/js/edit.js
@@ -11,6 +11,9 @@ $("#event-start-input-time").blur(update_starttime_for_event);
 $("#eventtitle").blur(update_title_for_event);
 $("#gcal").blur(update_gcal_for_event);
 $("#contact").blur(update_contact_for_event);
+
+$("#summaryeditbutton").on("click", summary_edit_save_button);
+$("#summarycancelbutton").on("click", summary_cancel_button);
 $.getJSON("/events/"+get_current_event_id()+"/summary", function(data) {
     $("#summary").html(markdown.toHTML(data.summary));
 });
